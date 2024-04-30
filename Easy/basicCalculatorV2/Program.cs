e1601ffeb536af73abb79e4a0a8b03af411a61d5
sstankov <stankovst@gmail.com>
2024-04-30 21:08:04 +0300

    adding basicCalculatorV2

---
+++ Easy/basicCalculatorV2/Program.cs
@@ -0,0 +1,33 @@
+﻿/* create a simple program that performs basic math, usch as adding/subtracting 2 integers. */
+
+using System;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+        //creating integers from user input:
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter 2nd number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        //prompting user to select operation:
+        Console.WriteLine("Enter which operation to perform : Add, Subtract, or Multiply");
+        string mathOperator = Console.ReadLine();
+        //if the selected opration to perform is addition:
+        if (mathOperator == "add" || mathOperator == "Add" || mathOperator == "ADD")
+            //print the sum output:
+            Console.WriteLine(num1 + num2);
+        //otherwise if the selected operation is subtraction, print the output from the subtraction:
+        else if ((mathOperator == "Subtract" || mathOperator == "subtract" || mathOperator == "SUBTRACT"))
+            Console.WriteLine(num1 - num2);
+        //else, print the output from the multiplication:
+        else
+        {
+            Console.WriteLine(num1 * num2);
+        }
+
+    }
+}
