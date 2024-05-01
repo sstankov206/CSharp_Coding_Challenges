ea0756535b38229ad9e80a96dd527d92e99999cc
sstankov <stankovst@gmail.com>
2024-05-01 19:31:08 +0300

    adding AgeToDays

---
+++ Easy/AgeToDays/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Create a function that takes the age in years and returns the age in days.
+
+Examples:
+
+CalcAge(65) ➞ 23725
+
+CalcAge(0) ➞ 0
+
+CalcAge(20) ➞ 7300
+
+*/
+
+using System;
+
+class Program {
+
+    public static void Main(string[] args) {
+
+        int year = 365;
+        //reading input:
+        int age = int.Parse(Console.ReadLine());
+        int ageConverted = age * year;
+        //printing output:
+        Console.WriteLine(ageConverted);
+
+
+    }
+
+
+}
