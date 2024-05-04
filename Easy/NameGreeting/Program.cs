5525745988f01b8375e2ec46e2784462e0369600
sstankov <stankovst@gmail.com>
2024-05-04 23:18:19 +0300

    ADD: Exercise 'NameGreeting'

---
+++ Easy/NameGreeting/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Create a function that takes a name and returns a greeting in the form of a string.
+
+Examples
+HelloName("Gerald") ➞ "Hello Gerald!"
+
+HelloName("Tiffany") ➞ "Hello Tiffany!"
+
+HelloName("Ed") ➞ "Hello Ed!"
+
+*/
+
+using System;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+       //reading input:
+       string name = Console.ReadLine();
+
+        //printing output:
+        Console.WriteLine(NameGreeting(name));
+    }
+
+    //creating method that creates the greeting:
+    public static string NameGreeting(string name) {
+        return ($"Hello " + name + "!");
+    
+    }
+}
+
+
