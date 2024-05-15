c5206d29181f595e39e65ab694cc9359636c8b4d
sstankov <stankovst@gmail.com>
2024-05-15 21:53:18 +0300

    ADD: Exercise 'Reverse A String'

---
+++ Easy/ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {//reading input:
+        string input = Console.ReadLine();
+
+        //printing output:
+        Console.WriteLine(Reverse(input));
+    }
+
+    //method to reverse the string:
+    public static string Reverse(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
