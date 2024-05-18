990f4668be308ceb12cf73dabe0c9872f157f0ba
sstankov <stankovst@gmail.com>
2024-05-18 19:42:42 +0300

    ADD: Execise 'ReturnFirstInArr'

---
+++ Easy/ReturnFirstInArr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //reading input:
+        string[] input = Console.ReadLine().Split(",");
+
+        //printing output:
+        Console.WriteLine(GetFirstElement(input));
+    }
+
+    //method for returning the first element from the array:
+    public static string GetFirstElement(string[] input)
+    {
+        return input.FirstOrDefault();
+    }
+
+}
