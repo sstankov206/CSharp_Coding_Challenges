3e71b785a0a3690cbd91466af2d7a465647c0b1f
sstankov <stankovst@gmail.com>
2024-05-20 08:44:26 +0300

    ADD: Exercise 'Articles'

---
+++ OOP/Articles/Article.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Articles
+{
+    public class Article
+    {
+        //properties:
+        public string Title { get; set; }
+        public string Content{ get; set; }
+        public string Author{ get; set; }
+
+        //constructor:
+        public Article(string title, string content, string author)
+        {
+             this.Author = author;
+            this.Content = content;
+            this.Title = title;
+        }
+        
+        //method for "Edit" command - new content:
+        public void Edit(string content)
+        {
+            this.Content = content;
+        }
+
+        //method for "ChangeAuthor" command - new author:
+        public void ChangeAuthor(string author) 
+        {
+            this.Author = author;
+        }
+
+        //method for "Rename" command - new title:
+        public void Rename(string title)
+        {
+            this.Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ OOP/Articles/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Create a class Article with the following properties:
+
+· Title – a string
+
+· Content – a string
+
+· Author – a string
+
+The class should have a constructor and the following methods:
+
+· Edit (new content) – change the old content with the new one
+
+· ChangeAuthor (new author) – change the author
+
+· Rename (new title) – change the title of the article
+
+· Override the ToString method – print the article in the following format:
+
+"{title} - {content}: {autor}"
+
+Write a program that reads an article in the following format "{title}, {content}, {author}". On the next line,
+you will receive a number n, representing the number of commands, which will follow after it. On the next n lines, 
+you will be receiving the following commands: "Edit: {new content}"; "ChangeAuthor: {new author}"; "Rename: {new title}". 
+At the end, print the final state of the article.
+
+*/
+
+using Articles;
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    { //reading input:
+        var input = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+        Article article = new Article(input[0], input[1], input[2]); //author, content, title
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            //reading command from input:
+            var command = Console.ReadLine().Split(": ",
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            switch (command[0])
+            {
+                case "Edit":
+                    article.Edit((command[1]));
+                    break;
+                case "ChangeAuthor":
+                    article.Edit((command[1]));
+                    break;
+                case "Rename":
+                    article.Edit((command[1]));
+                    break;
+            }
+        }
+
+        //printing output:
+        Console.WriteLine(article.ToString);
+    }
+}
