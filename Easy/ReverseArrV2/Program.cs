cbac1dcf2305c745eec26fb961ae918a432fb4e4
sstankov <stankovst@gmail.com>
2024-05-23 15:10:36 +0300

    ADD: Exercise: 'Reverse the ArrayV2'

---
+++ Easy/ReverseArrV2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Problem: Reverse the Array
+
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //reading input of integers, storing them in original array called "input" :
+        int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+        //printing output:
+        Console.WriteLine(ReverseArr(input));
+    }
+    //method to reverse the array:
+    public static string ReverseArr(int[] input)
+    {
+        Array.Reverse(input);
+        return string.Join(" ", input);
+    }
+}
