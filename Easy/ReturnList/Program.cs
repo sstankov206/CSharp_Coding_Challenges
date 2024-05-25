48245d5f654ff87a0477b98937e4a61714e38c1b
sstankov <stankovst@gmail.com>
2024-05-25 16:24:41 +0300

    ADD: Exercise 'Return a List'

---
+++ Easy/ReturnList/Program.cs
@@ -0,0 +1,33 @@
+﻿// Practice: Returning a List in C#
+
+using System;
+using System.Reflection.Metadata.Ecma335;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter text, seprated by space:");
+
+        //reading input from user:
+        var input = Console.ReadLine().
+            Split(new char[] { ' ', ',' },
+            StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+        //printing output:
+        Console.WriteLine(ReturnList(input));
+
+
+    }
+
+    //method that returns a list:
+    public static string ReturnList(List<string> input)
+    {
+        List<string> list = new List<string>();
+        return String.Join(" ", input);
+    }
+
+
+ 
+
+}
