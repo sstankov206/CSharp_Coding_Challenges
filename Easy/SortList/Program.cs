39b64dead8c77bc7739745132fd2277b445c923c
sstankov <stankovst@gmail.com>
2024-05-26 19:05:00 +0300

    ADD: Exercise 'Sorting a List'

---
+++ Easy/SortList/Program.cs
@@ -0,0 +1,25 @@
+﻿// Practice: Sorting a List in C#
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter numbers, separated by space:");
+
+        //reading input from user:             Ex: 5 3 8 10
+        var input = Console.ReadLine().
+            Split(new char[] { ' ', ',' },
+            StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        //sorting the list:                   Ex: 3 5 8 10
+        input.Sort();
+
+        //printing output:
+        Console.WriteLine(String.Join(" ", input));
+
+    }
+}
