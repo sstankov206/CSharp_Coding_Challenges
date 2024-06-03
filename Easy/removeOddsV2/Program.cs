ce2fd9feafc1a079ef04844f20bc8603984ed22a
sstankov <stankovst@gmail.com>
2024-06-03 20:15:36 +0300

    ADD: Exercise 'Remove Odd Numbers V2'

---
+++ Easy/removeOddsV2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Practice: Remove odd numbers from a list of numbers, using LINQ arguments.
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //list of numbers:
+        List<int> numbers = Console.ReadLine().Split(" ")
+            .Select(int.Parse)
+            .ToList();
+
+        //new list that stores even numbers:
+        List<int> evenNumbers = numbers.Where(number => number % 2 == 0).ToList();
+
+        //printing output:
+        Console.WriteLine(string.Join(" ", evenNumbers));
+
+    }
+}
