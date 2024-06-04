80d4bb0942794a9c8d3e03ed3166f88d9e88b727
sstankov <stankovst@gmail.com>
2024-06-04 20:16:00 +0300

    ADD: Exercise 'Gradebook'

---
+++ Easy/Gradebook/Program.cs
@@ -0,0 +1,30 @@
+﻿// Exercise: Create student's gradebook using Dictinary.
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+class Program 
+{
+    public static void Main(string[] args) 
+    {//enter number of students to record:
+    int input = int.Parse(Console.ReadLine());
+
+        //storing students in a dictionary:
+        Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+        //for the total number of students, create the follwing keys and values:
+        for (int i = 0; i < input; i++)
+        {
+            string student = Console.ReadLine(); //key - students
+            double grades = double.Parse(Console.ReadLine());// value - grades
+            students.Add(student, new List<double> { grades }); //adds each created student, and their grades
+
+            //printing output
+            foreach (var pair in students)
+            {
+                Console.WriteLine(pair.Key + ": " + string.Join(", ", pair.Value));
+            }
+        }
+
+    }
+
+}
