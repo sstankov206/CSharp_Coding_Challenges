af268671d837c3491d416e058040da11f90b6ddb
sstankov <stankovst@gmail.com>
2024-06-05 20:13:31 +0300

    ADD: Exercise 'Student Subjects'

---
+++ Easy/studentSubjects/Program.cs
@@ -0,0 +1,30 @@
+﻿// Practice: Create and display list of students and their subjects of study, using C# Dictionaries.
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {//enter number of students to record:
+        int input = int.Parse(Console.ReadLine());
+
+        //storing students in a dictionary:
+        Dictionary<string, List<string>> students = new Dictionary<string, List<string>>();
+
+        //for the total number of students, create the following keys and values:
+        for (int i = 0; i < input; i++)
+        {
+            string student = Console.ReadLine(); //key - students
+            string subjects = Console.ReadLine();// value - subjects:
+            students.Add(student, new List<string> {subjects}); //adds each created student, and their school subjects
+
+            //printing output
+            foreach (var pair in students)
+            {
+                Console.WriteLine(pair.Key + ": " + string.Join(", ", pair.Value));
+            }
+        }
+
+    }
+
+}
