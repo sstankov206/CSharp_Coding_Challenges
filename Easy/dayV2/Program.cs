188a9543a51bcb56af131303c462b79c7c966ee9
sstankov <stankovst@gmail.com>
2024-06-08 15:34:57 +0300

    ADD: 'Exercise Days V2'

---
+++ Easy/dayV2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Type a number 1 - 7:");
+        int input = int.Parse(Console.ReadLine());
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        Console.WriteLine($"It's {days[input - 1]}");
+    }
+}
