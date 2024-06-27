33b5a884fe99a5a8631b948219051ae901b02af5
sstankov <stankovst@gmail.com>
2024-06-27 16:26:55 +0300

    ADD: Exercise 'Index Of'

---
+++ Easy/IndexOf/Program.cs
@@ -0,0 +1,25 @@
+﻿//Practice: using the IndexOf method.
+
+using System;
+class Program 
+{
+    public static void Main(string[] args) 
+    {
+        string message = "(What if) there are (more than) one (set of parentheses)?";
+        while (true)
+        {
+            int openingPosition = message.IndexOf('(');
+            if (openingPosition == -1) break;
+
+            openingPosition += 1;
+            int closingPosition = message.IndexOf(')');
+            int length = closingPosition - openingPosition;
+            Console.WriteLine(message.Substring(openingPosition, length));
+
+            // Note the overload of the Substring to return only the remaining 
+            // unprocessed message:
+            message = message.Substring(closingPosition + 1);
+        }
+
+    }
+}
