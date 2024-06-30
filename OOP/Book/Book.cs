0d823596ca1f3c75a1415de2a5a2095a0d425bd5
sstankov <stankovst@gmail.com>
2024-06-30 17:47:56 +0300

    ADD: Exercise 'Book'

---
+++ OOP/Book/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookz
+{
+    public class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        //constructors:
+        public Book(string Title, string Author, int Pages)
+        {
+            this.title = Title;
+            this.author = Author;
+            this.pages = Pages;
+        }
+        public Book()
+        {
+                
+        }
+
+
+    }
+}
---
+++ OOP/Book/Program.cs
@@ -0,0 +1,20 @@
+﻿//Exercise: Practice creaing objects 
+
+using System;
+using Bookz;
+
+class Program
+{
+    static void Main(string[] args) 
+    {  //creating new instance of the Book class, from user input:
+        Book book1 = new Book();
+        book1.title = Console.ReadLine();
+        book1.author = Console.ReadLine();
+        book1.pages = int.Parse(Console.ReadLine());
+
+        //printing output:
+        Console.WriteLine(String.Join(", ", book1.title, book1.author, book1.pages));
+
+
+    }
+}
