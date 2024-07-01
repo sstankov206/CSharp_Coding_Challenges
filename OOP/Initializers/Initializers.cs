880b2688cd14055f0d4f1a9ca77828b2eed32694
sstankov <stankovst@gmail.com>
2024-07-01 15:51:31 +0300

    ADD: Exercise 'Initializers'

---
+++ OOP/Initializers/Initializers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initializers_
+{
+    public class Initializers
+    {
+
+        // Using object initializers
+
+        // Example: class that defines a Pet with a name and age
+        public class Pet
+        {
+            public string Name { get; set; } = "";
+            public int Age { get; set; } = 0;
+
+            public Pet() { }
+        }
+
+        public class Dog : Pet
+        {
+            public bool IsTrained { get; set; } = false;
+
+            public Dog() { }
+        }
+
+        public class Cat : Pet
+        {
+            public bool IsDeclawed { get; set; } = false;
+
+            public Cat() { }
+        }
+
+        // Define a class that implements an internal collection
+        public class PetOwner
+        {
+            public string Name { get; set; } = "";
+
+            public List<Pet>? Pets { get; set; }
+        }
+    }
+
+}
+
---
+++ OOP/Initializers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Initializers_;
+using static Initializers_.Initializers;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Using object and collection initializers
+
+        // Use the initializer syntax to create new objects
+        Dog dog = new Dog { Name = "Michael", Age = 10, IsTrained = true };
+        Cat cat = new Cat { Name = "Mima", Age = 12, IsDeclawed = false };
+
+        Console.WriteLine($"Dog: {dog.Name}, {dog.Age}");
+        Console.WriteLine($"Cat: {cat.Name}, {cat.Age}");
+
+        // Initializers can be used on anonymous types
+        var pet = new { Name = "Eeyore", Age = 9 };
+        Console.WriteLine($"{pet.Name}, {pet.Age}");
+
+        // Initialize a collection with a set of objects
+        PetOwner owner = new PetOwner
+        {
+            Name = "Stanimir S.",
+            Pets = new List<Pet> {
+        new Dog {Name = "Michael", Age = 10},
+        new Cat {Name = "Mima", Age = 12 },
+        new Dog {Name = "Eeyore", Age = 9}
+    }
+        };
+
+        Console.WriteLine($"{owner.Name}'s Pets:");
+        foreach (Pet p in owner.Pets)
+        {
+            Console.WriteLine($"{p.Name}");
+        }
+
+    }
+}
+
