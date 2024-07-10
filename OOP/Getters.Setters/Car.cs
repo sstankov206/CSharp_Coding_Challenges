b95c7a0a9b7664a35aa37165cc759be32317b3d4
sstankov <stankovst@gmail.com>
2024-07-10 14:45:18 +0300

    ADD: Exercise 'Getters & Setters'

---
+++ OOP/Getters.Setters/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters.Setters
+{
+    public class Car
+    {
+        private int speed;
+
+        public Car(int speed)
+        {
+            Speed = speed;
+        }
+
+        //Properties (getters & setters):
+        public int Speed
+        {
+            get { return speed; } //read
+            set //write
+            {
+                if (speed > value)
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+
+        }
+    }
+}
---
+++ OOP/Getters.Setters/Program.cs
@@ -0,0 +1,15 @@
+﻿// Exercise: Practice using Getters & Setters acccessors within properties.
+
+using System;
+using Getters.Setters;
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        //creating new instance of the Car class:
+        Car car = new Car(200);
+
+        //getting the car's speed via the Properties accessors (getters & setters):
+        Console.WriteLine(car.Speed);
+    }
+}
