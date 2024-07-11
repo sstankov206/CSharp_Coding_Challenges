6dbb56e607f08d350360b03c9d55b1c2787c1ed2
sstankov <stankovst@gmail.com>
2024-07-11 18:09:19 +0300

    ADD: Exercise 'Constructor'

---
+++ OOP/Constructor/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_
+{
+    public class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        public string color;
+        public int topspeed;
+        public int horsepower;
+
+        // this is what a constructor looks like:
+        public Car(string make, string model, int year, string color, int topseed, int horsepower)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+            this.topspeed = topseed;
+            this.horsepower = horsepower;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"You drive the {color} {year} {make} {model}.");
+        }
+    }
+}
---
+++ OOP/Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿// Exercise: Practice using Constructors within OOP.
+
+using Constructor_;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //creating a new instance of the Car class with constructor:
+        Car fordmustang = new Car("Ford", "Mustang", 2018, "black", 160, 450);
+
+        fordmustang.Drive();
+
+        Console.WriteLine($"Car specs: \n top mph: {fordmustang.topspeed} \n hp: {fordmustang.horsepower}");
+    }
+}
