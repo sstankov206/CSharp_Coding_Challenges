b302e72c27b1869295c4d5d37895466a79066e78
sstankov <stankovst@gmail.com>
2024-07-19 19:39:41 +0300

    ADD: Exercise 'Abstraction'

---
+++ OOP/Abstraction/Abstraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract class Animal
+    {
+        // Abstract method (does not have a body)
+        public abstract void animalSound();
+        // Regular method
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    // Derived class (inherit from Animal)
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+}
---
+++ OOP/Abstraction/Program.cs
@@ -0,0 +1,14 @@
+﻿// Exercise: Practice using Abstraction within OOP.
+
+using System;
+using Abstraction;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pig myPig = new Pig();  // Create a Pig object
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
