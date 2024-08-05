2b50c7175fdf0a7ebbf33d8ed117e8da15d81900
sstankov <stankovst@gmail.com>
2024-08-05 17:12:57 +0300

    ADD: 'Lab: Inheritance'

---
+++ OOP-Projects/Lab.Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿// Lab: Inheritace
+//Requirements: https://judge.softuni.org/Contests/1499/Inheritance-Lab
+
+using System;
+using Farm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //instances of the created classes:
+
+        Dog dog = new Dog();
+        dog.Bark();
+        dog.Eat();
+
+        Puppy puppy = new Puppy();
+        puppy.Eat();
+        puppy.Bark();
+        puppy.Weep();
+
+        Cat cat = new Cat();
+        cat.Eat();
+        cat.Meow();
+
+    }
+}
---
+++ OOP-Projects/Lab.Inheritance/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Farm
+{
+    public class Startup
+    {
+
+    }
+
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    public class Dog : Animal //Dog inherits from Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+
+    public class Puppy : Dog //Puppy inherits from Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("Weeping...");
+        }
+    }
+
+    public class Cat : Animal  //Cat inherits from Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Meowing...");
+        }
+    }
+}
