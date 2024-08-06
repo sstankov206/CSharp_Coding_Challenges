07fd86215893b744ab22fbadba758f9044a4e9ec
sstankov <stankovst@gmail.com>
2024-08-06 21:08:14 +0300

    ADD: random list

--- OOP-Projects/Lab.Inheritence.Ex4/Program.cs
+++ OOP-Projects/Lab.Inheritence.Ex4/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Lab.Inheritence.Ex4;
+
 Console.WriteLine("Hello, World!");
+
+RandomList randomList = new RandomList();
+
+randomList.Add("RandomSting");
+randomList.Add("RandomSting1");
+randomList.Add("RandomSting3");
+randomList.Add("RandomSting2");
+
+Console.WriteLine(string.Join(", ", randomList));
+
+randomList.Remove();
+
+Console.WriteLine(string.Join(", ", randomList));
