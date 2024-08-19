0c7531aa5251b003a2698847eed69fd163458dda
sstankov <stankovst@gmail.com>
2024-08-19 16:40:24 +0300

    initial commit, pending review

---
+++ OOP-Projects/Lab.Encapsulation.Ex2.SalaryIncrease/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab.Encapsulation.Ex2.SalaryIncrease
+{
+    public class Person
+    {
+        //properties (public) with private setters:
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        // new property for this exercise:
+
+        public decimal Salary { get; private set; }
+
+        //constructor, expanded to include new property - Salary:
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        //new method for this lab - IncreaseSalary
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30) //if the person is over 30 years of age:
+            {
+                this.Salary += this.Salary * percentage / 100; //increase salary by provided percentage.
+            }
+            else //else if the person is <30 years of age:
+            {
+                this.Salary += this.Salary * percentage / 200; //increase salary by half of the percentage.
+            }
+        }
+
+        //override ToString(), modified from last lab to include the methods and properties for this lab :
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary} leva.";
+        }
+    }
+}
---
+++ OOP-Projects/Lab.Encapsulation.Ex2.SalaryIncrease/Program.cs
@@ -0,0 +1,45 @@
+﻿/*  SALARY INCREASE:
+ *  
+ *  Refactor the project from the last lab - "Lab Encapsulation, Exercise 1: Sort People by Name & Age".
+ *  
+ *  Create objects of the class Person. Read their name, age, and salary from the console.
+ *  Read the percentage of the bonus to every Person's salary.
+ *  People younger than 30 get half the increase. 
+ *  Expand Person from the previous task.
+
+New properties and methods:
+    • Salary: decimal
+    • IncreaseSalary(decimal percentage)
+*/
+
+using System;
+using Lab.Encapsulation.Ex2.SalaryIncrease;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating user input:
+        Console.WriteLine("How many people to enter? :");
+        var lines = int.Parse(Console.ReadLine());
+
+        var persons = new List<Person>();
+        for (int i = 0; i < lines; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            var person = new Person(cmdArgs[0],
+                                    cmdArgs[1],
+                                    int.Parse(cmdArgs[2]),
+                                    decimal.Parse(cmdArgs[3])); //records each person's first+last name, age & salary from user input.
+
+            persons.Add(person); //adds each person to list
+        }
+        Console.WriteLine("Percentage % for salary increase :");
+        var percentage = decimal.Parse(Console.ReadLine()); //reads percentage of salary increase from user input
+        persons.ForEach(p => p.IncreaseSalary(percentage));
+        persons.ForEach(p => Console.WriteLine(p.ToString())); //performs salary increase calculation, prints output in requested format.
+
+
+    }
+}
+
+
