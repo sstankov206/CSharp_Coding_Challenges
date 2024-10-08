8bff7117dc6d7395a812f747dd331d143c270fda
sstankov <stankovst@gmail.com>
2024-10-08 19:32:39 +0300

    ADD: Proj. TaskManager, initial commit

---
+++ OOP-Projects/TaskManager/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+
+namespace Task_Manager.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task(int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            IsCompleted = false;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Title} - {(IsCompleted ? "Completed" : "Pending")}\nDescription: {Description}";
+        }
+    }
+}
---
+++ OOP-Projects/TaskManager/Program.cs
@@ -0,0 +1,13 @@
+﻿//Description: Simple Task Manager app that implements OOP principles
+// and is able to create, update, delete tasks.
+
+using System;
+using Task_Manager.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        
+    }
+}
