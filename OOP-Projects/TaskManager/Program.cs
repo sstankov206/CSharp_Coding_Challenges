5fc2247019ced9681b2f747c5ff72b39bee89764
sstankov <stankovst@gmail.com>
2024-10-10 23:53:39 +0300

    ADD: Proj. TaskManager, update 2

--- OOP-Projects/TaskManager/Program.cs
+++ OOP-Projects/TaskManager/Program.cs
@@ -7,7 +7,54 @@ using Task_Manager.Models;
 class Program
 {
     static void Main(string[] args)
-    { 
-        
+    {
+        TaskManager taskManager = new TaskManager();
+        string command;
+
+        do
+        {
+            Console.WriteLine("Enter a command (add, remove, list, complete, exit):");
+            command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "add":
+                    Console.Write("Enter task title: ");
+                    string title = Console.ReadLine();
+                    Console.Write("Enter task description: ");
+                    string description = Console.ReadLine();
+                    taskManager.AddTask(title, description);
+                    break;
+
+                case "remove":
+                    Console.Write("Enter task ID to remove: ");
+                    if (int.TryParse(Console.ReadLine(), out int removeId))
+                    {
+                        taskManager.RemoveTask(removeId);
+                    }
+                    break;
+
+                case "list":
+                    taskManager.ListTasks();
+                    break;
+
+                case "complete":
+                    Console.Write("Enter task ID to complete: ");
+                    if (int.TryParse(Console.ReadLine(), out int completeId))
+                    {
+                        taskManager.CompleteTask(completeId);
+                    }
+                    break;
+
+                case "exit":
+                    Console.WriteLine("Exiting...");
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown command.");
+                    break;
+            }
+
+        } while (command != "exhit");
     }
 }
