4f7e27a25ad3705c45a10f97abb456b081d17732
sstankov <stankovst@gmail.com>
2024-10-14 16:54:57 +0300

    ADD: Proj. EmployeeUserAccountCreation, initial commit

---
+++ OOP-Projects/EmployeeUserAccountCreation/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeUserAccountCreation.Models
+{
+    public class Employee
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Position { get; private set; }
+
+        public Employee(string name, string email, string position)
+        {
+            Name = name;
+            Email = email; 
+            Position = position;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Email: {Email}, Position: {Position}";
+        }
+    }
+}
---
+++ OOP-Projects/EmployeeUserAccountCreation/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Description: This application's purpose is to showcase a simple C#
+ * application, that implements OOP principles and is able to 
+ * create new emlpoyee accounts for a fictinal firm, and assign
+ * employee attributes sich as name, email, and job position
+ */
+
+using System;
+using EmployeeUserAccountCreation.Models;
+class Program
+{
+    static void Main(string[] args)
+    { 
+        
+    }
+}
