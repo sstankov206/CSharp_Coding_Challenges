a4cd6ac3a53256c6ee54287bb9d92203a58bcaae
sstankov <stankovst@gmail.com>
2024-10-21 22:18:13 +0300

    ADD: Proj. StellarLifecycleSimulator update 1

--- OOP-Projects/StellarLifeCycleSimulator/Program.cs
+++ OOP-Projects/StellarLifeCycleSimulator/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using StellarLifeCycleSimulator.Models;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Star Simulations
+        Star sun = new Star("Sun", "G-type");
+        sun.Shine();
+
+        for (int i = 0; i < 10; i++) // Simulate 10 billion years
+        {
+            sun.GrowOlder();
+            sun.Shine();
+        }
+
+        sun.Supernova();
+
+        Star unknownStar = new Star("Mystery Star");
+        unknownStar.Shine();
+    }
+}
