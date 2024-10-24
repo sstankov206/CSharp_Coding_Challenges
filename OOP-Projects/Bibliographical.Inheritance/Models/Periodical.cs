a57389f72417bc323f54f3002cddb52ae7ba9c9d
sstankov <stankovst@gmail.com>
2024-10-24 18:49:21 +0300

    ADD: Proj. Bibliographical.Inheritance, update 1

---
+++ OOP-Projects/Bibliographical.Inheritance/Models/Periodical.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bibliographical.Inheritance.Models
+{
+    public class Periodical: Resource
+    {
+        public string Period { get; set; }
+
+        public Periodical(string title, string category, string period)
+            : base(title, category)
+        {
+            Period = period;
+        }
+
+        public override void UpdateStatus()
+        {
+            Status = Status == "In-use" ? "Available" : "In-use";
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"Period: {Period}");
+        }
+    }
+}
