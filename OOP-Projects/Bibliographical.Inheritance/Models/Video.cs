29e9c148ee618b33011e2dc5bfb3b5f72d0d12ef
sstankov <stankovst@gmail.com>
2024-10-25 19:19:33 +0300

    ADD: Proj. Bibliographical.Inheritance, update 2

---
+++ OOP-Projects/Bibliographical.Inheritance/Models/Video.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bibliographical.Inheritance.Models
+{
+    public class Video: Resource
+    {
+        public string Director { get; set; }
+        public int Runtime { get; set; }
+        public string MediaType { get; set; }
+
+        public Video(string title, string category, string director, int runtime, string mediaType)
+            : base(title, category)
+        { 
+            Director = director;
+            Runtime = runtime;
+            MediaType = mediaType;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"Director: {Director}");
+            Console.WriteLine($"runtime: {Runtime} minutes");
+            Console.WriteLine($"Media Type: {MediaType}");
+        }
+    }
+}
