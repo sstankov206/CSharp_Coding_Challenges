947a86e0dc9f7225482fd398aa371508e26d182d
sstankov <stankovst@gmail.com>
2024-10-26 00:27:31 +0300

    ADD: Proj. Bibliographical.Inheritance, update 3

--- OOP-Projects/Bibliographical.Inheritance/Program.cs
+++ OOP-Projects/Bibliographical.Inheritance/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using Bibliographical.Inheritance.Models;
-
 class Program
 {
     static void Main(string[] args)
-    { 
-        
+    {
+        // Testing Resource
+        Resource test = new Resource("Example Title", "General");
+        test.GetInfo();
+        test.UpdateStatus();
+        Console.WriteLine($"Updated Status: {test.UpdateStatus}\n");
+
+        // Testing Book
+        string[] authors = { "Charles Petzold" };
+        Book book = new Book("Code: The Hidden Language of Computer Hardware and Software", "Non-Fiction", authors, 396);
+        book.GetInfo();
+        Console.WriteLine();
+
+        // Testing Periodical
+        Periodical periodical = new Periodical("Wired", "Technology", "Monthly");
+        periodical.GetInfo();
+        periodical.UpdateStatus();
+        Console.WriteLine($"Updated Status: {periodical.UpdateStatus}\n");
+
+        // Testing Video
+        Video video = new Video("Ex Machina", "Sci-Fi", "Alex Garland", 108, "On-Demand");
+        video.GetInfo();
     }
 }
