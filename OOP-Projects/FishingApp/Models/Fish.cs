4ed8cefb080b3d85baa9d68e689f050ce106e73a
sstankov <stankovst@gmail.com>
2024-10-29 18:03:29 +0200

    ADD: Proj. FishingApp, initial commit

---
+++ OOP-Projects/FishingApp/Models/Fish.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace FishingApp.Models
+{
+    //Base clas:
+    public class Fish
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public string Habitat { get; set; }
+
+        public Fish(string name, double weight, string habitat)
+        {
+            Name = name;
+            Weight = weight;
+            Habitat = habitat;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Fish: {Name}, Weight: {Weight} lbs, Habitat: {Habitat}");
+        }
+    }
+}
---
+++ OOP-Projects/FishingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Simple fishing app with OOP, which allows
+ * users to manage different types of fish and 
+ * fishing trips.
+ */
+
+using System;
+using FishingApp.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+    
+    }
+}
