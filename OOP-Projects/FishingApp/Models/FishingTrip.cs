7aba5b039a6d49b5f7c2457ca87e4c5800a87278
sstankov <stankovst@gmail.com>
2024-10-31 00:34:32 +0200

    ADD: Proj. Fishing App, update 2

---
+++ OOP-Projects/FishingApp/Models/FishingTrip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FishingApp.Models
+{
+    public class FishingTrip
+    {
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public List<Fish> CaughtFish { get; set; }
+
+        public FishingTrip(string location, DateTime date)
+        {
+            Location = location;
+            Date = date;
+            CaughtFish = new List<Fish>();
+        }
+
+        public void AddFish(Fish fish)
+        {
+            CaughtFish.Add(fish);
+        }
+
+        public void DisplayTripDetails()
+        {
+            Console.WriteLine($"Fishing Trip Location: {Location}, Date: {Date.ToShortDateString}");
+            Console.WriteLine($"Caught Fish:");
+            foreach (var fish in CaughtFish)
+            {
+                fish.DisplayInfo();
+                Console.WriteLine();
+            }
+        }
+    }
+}
