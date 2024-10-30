f5f0c128a1c80bab68712fefa96fd993b5aaed1f
sstankov <stankovst@gmail.com>
2024-10-30 18:53:12 +0200

    ADD: Proj. FishingApp, update 1

---
+++ OOP-Projects/FishingApp/Models/FreshwaterFish.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FishingApp.Models
+{
+    //Derived class:
+
+    public class FreshwaterFish : Fish
+    {
+        public string FreshwaterType {  get; set; }
+        public FreshwaterFish(string name, double weight, string habitat, string freshwaterType)
+        :base(name, weight, habitat)
+        {
+            FreshwaterType = freshwaterType;
+        }
+
+        public override void DisplayInfo()
+        { 
+            base.DisplayInfo();
+            Console.WriteLine($"Freshwater Type: {FreshwaterType}");
+        }
+    }
+}
---
+++ OOP-Projects/FishingApp/Models/SaltwaterFish.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FishingApp.Models
+{
+    //Another Derived Class:
+    public class SaltwaterFish : Fish
+    {
+        public string SaltwaterType { get; set; }
+        public SaltwaterFish(string name, double weight, string habitat, string saltwaterType)
+            : base(name, weight, habitat)
+        {
+            SaltwaterType = saltwaterType;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Saltwater Type: {SaltwaterType}");
+        }
+
+    }
+}
