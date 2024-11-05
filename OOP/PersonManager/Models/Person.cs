4bb643938ca4f0e09df91d45a3181ce5ba7fb6e8
sstankov <stankovst@gmail.com>
2024-11-05 19:04:31 +0200

    ADD: Ex. PersonManager, initial commit

---
+++ OOP/PersonManager/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PersonManager.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }   
+        public string City { get; set; }
+
+        public Person(string name, int age, string city)
+        {
+            Name = name;
+            Age = age;
+            City = city;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, Age: {Age}, City: {City}";
+        }
+
+    }
+}
---
+++ OOP/PersonManager/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Simple app with C# OOP and lambda expressions,
+ * that manages a list of objects of the 'Person' class.
+ * It performs the following operations:
+ * - Adding a 'Person'
+ * - Displaying a list of people.
+ * - Soring the list based on different criteria
+ * (name, age,etc., using lambda expressions).
+ * 
+ */
+
+using System;
+using System.Linq;
+using PersonManager.Models;
+
+class Program
+{ 
+
+}
