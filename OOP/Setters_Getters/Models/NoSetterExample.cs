c852957202f432dea5b1a3bce50d746505982655
sstankov <stankovst@gmail.com>
2024-11-11 23:36:57 +0200

    ADD: Ex. Setters & Getters, update 1

--- OOP/Setters_Getters/Models/NoSetterExample.cs
+++ OOP/Setters_Getters/Models/NoSetterExample.cs
@@ -6,5 +6,16 @@ namespace Setters_Getters.Models
 {
     public class NoSetterExample
     {
+        public string Name { get; }
+
+        public NoSetterExample(string name)
+        {
+            Name = name;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name (No Setter): {Name}");
+        }
     }
 }
