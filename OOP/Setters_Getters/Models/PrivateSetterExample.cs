638ee27736a944fccaa56891853bbadb692f2a3e
sstankov <stankovst@gmail.com>
2024-11-12 19:29:22 +0200

    ADD: Ex. Setters & Getters, update 2

---
+++ OOP/Setters_Getters/Models/PrivateSetterExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Setters_Getters.Models
+{
+    public class PrivateSetterExample
+    {
+        public string Name { get; private set; }
+
+        public PrivateSetterExample(string name)
+        {
+            Name = name;
+        }
+
+        public void ChangeName(string newName)
+        {
+            Name = newName; // Allowed because the setter is private within the class.
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name (Private Setter): {Name}");
+        }
+    }
+}
