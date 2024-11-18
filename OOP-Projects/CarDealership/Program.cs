7534cba30068ab8bb52123efc39c3dfd9ff350f6
sstankov <stankovst@gmail.com>
2024-11-18 22:42:52 +0200

    ADD: Proj. CarDealership, initial commit

---
+++ OOP-Projects/CarDealership/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Car Dealership App
+ * 
+ * The app will:
+ * 
+ * Show all available cars in stock.
+ * Allow the user to filter cars based on criteria
+ * like price, model, etc.
+ * Let the user place an order.
+ * Let the user contact the sales department 
+ * for more information.
+ */
+
+using System;
+using CarDealership.Models;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+
+    }
+}
