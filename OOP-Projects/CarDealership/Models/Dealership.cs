6b76a23288bde6e091067a5df50e0384b83ca88b
sstankov <stankovst@gmail.com>
2024-11-21 19:37:59 +0200

    ADD: Proj. CarDealership, update 2

---
+++ OOP-Projects/CarDealership/Models/Dealership.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarDealership.Models
+{
+    public class Dealership
+    {
+        public List<Car> Inventory { get; set; }
+
+        public Dealership()
+        {
+            Inventory = new List<Car>();
+        }
+    }
+}
