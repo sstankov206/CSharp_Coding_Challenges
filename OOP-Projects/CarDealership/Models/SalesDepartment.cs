929d764d2e7a306c33c08b3182129665abc91eac
sstankov <stankovst@gmail.com>
2024-11-29 17:54:01 +0200

    ADD: Proj. CarDealership, update 7

---
+++ OOP-Projects/CarDealership/Models/SalesDepartment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarDealership.Models
+{
+    public class SalesDepartment
+    {
+        public void RespondToInquiry(string customerName, string message)
+        {
+            Console.WriteLine($"Sales Department responding to {customerName}: {message}");
+            // Logic to respond to a customer's inquiry
+        }
+    }
+}
