c2dbc2a400ede50fa09224da3d3b3394fb7a86d0
sstankov <stankovst@gmail.com>
2024-12-05 19:30:25 +0200

    ADD: Proj. Superheroes, initial update

---
+++ OOP-Projects/Superheroes/Models/Superhero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Superheroes.Models
+{
+    public enum CharacterType { Hero, Villain }
+    public enum Universe { Marvel, DC }
+    public enum RaceType { Human, Alien, Demigod, Spirit, Other }
+    public class Superhero
+    {
+        public string Name { get; set; }
+        public Universe Universe { get; set; }
+        public string Hometown { get; set; }
+        public int? Age { get; set; }  // Nullable if unknown
+        public string Superpower { get; set; }
+        public CharacterType CharacterType { get; set; }
+        public RaceType RaceType { get; set; }
+        public List<Movie> Movies { get; set; }
+        public List<ComicBook> Comics { get; set; }
+        public List<string> CatchPhrases { get; set; }
+    }
+}
---
+++ OOP-Projects/Superheroes/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Console app with OOP in C#, designed to display
+ * info about Superheroes (Marvel vs DC.).
+ * The user should be able to query and filter the app,
+ * based on the following characteristics:
+ * - Name
+ * - Age
+ * - Universe
+ * - Hometown
+ * - Character Type
+ * - Race Type
+ * - List of movies released about hero
+ * - List of comic books released about hero
+ * - List of catch phrases of hero
+ */
+
+using System;
+using Superheroes.Models;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
