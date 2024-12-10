c317def07a1f133357849f7ab8fe42ebacea7540
sstankov <stankovst@gmail.com>
2024-12-10 18:41:17 +0200

    ADD: Proj. Superheroes, update 3

---
+++ OOP-Projects/Superheroes/Models/SuperheroManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Superheroes.Models
+{
+    public class SuperheroManager
+    {
+        public List<Superhero> Superheroes { get; set; }
+
+        public SuperheroManager()
+        {
+            Superheroes = new List<Superhero>();
+        }
+
+        // Add a superhero
+        public void AddSuperhero(Superhero superhero)
+        {
+            Superheroes.Add(superhero);
+        }
+    }
+}
