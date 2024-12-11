ed0734fa4f2b055246268031205abb11bb6cd963
sstankov <stankovst@gmail.com>
2024-12-11 17:53:11 +0200

    ADD: Proj. Superheroes, update 4

--- OOP-Projects/Superheroes/Models/SuperheroManager.cs
+++ OOP-Projects/Superheroes/Models/SuperheroManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using static Superheroes.Models.Superhero;
 
 
 namespace Superheroes.Models
@@ -18,6 +19,48 @@ namespace Superheroes.Models
         public void AddSuperhero(Superhero superhero)
         {
             Superheroes.Add(superhero);
+        }
+
+        // Filter superheroes by Universe (Marvel/DC)
+        public List<Superhero> FilterByUniverse(Universe universe)
+        {
+            return Superheroes.Where(s => s.Universe == universe).ToList();
+        }
+
+        // Filter superheroes by Character Type (Hero/Villain)
+        public List<Superhero> FilterByCharacterType(CharacterType characterType)
+        {
+            return Superheroes.Where(s => s.CharacterType == characterType).ToList();
+        }
+
+        // Filter superheroes by Age
+        public List<Superhero> FilterByAge(int age)
+        {
+            return Superheroes.Where(s => s.Age == age).ToList();
+        }
+
+        // Filter superheroes by Hometown
+        public List<Superhero> FilterByHometown(string hometown)
+        {
+            return Superheroes.Where(s => s.Hometown == hometown).ToList();
+        }
+
+        // Get movies of a superhero
+        public List<Movie> GetMovies(Superhero superhero)
+        {
+            return superhero.Movies;
+        }
+
+        // Get comics of a superhero
+        public List<ComicBook> GetComics(Superhero superhero)
+        {
+            return superhero.Comics;
+        }
+
+        // Get superheroes by Catchphrase
+        public List<Superhero> GetByCatchPhrase(string catchphrase)
+        {
+            return Superheroes.Where(s => s.CatchPhrases.Contains(catchphrase)).ToList();
         }
     }
 }
